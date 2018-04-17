758c338ddf307720d4cf0f859e30a9e55692959a
Mr.Mertilien <marcusmertilien@gmail.com>
2018-04-16 19:12:09 -0700

    Creation

---
+++ Study Sesh/Study Sesh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study Sesh
+//
+//  Created by Marcus Mertilien on 4/16/18.
+//  Copyright © 2018 Mertilien Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
