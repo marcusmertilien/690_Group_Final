41359211fa2bc6ec40023a08f771183eb063e145
shymoj <smojtaba@mail.sfsu.edu>
2018-04-29 19:28:37 -0700

    login firebase
    Added LoginViewContoller and podfile for firebase. Not tested, some log error makes it not run

--- Study Sesh/Study Sesh/AppDelegate.swift
+++ Study Sesh/Study Sesh/AppDelegate.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    class func configure(){
+    FirebaseApp.configure()
+    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
