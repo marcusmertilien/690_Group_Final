8890d92a495b5c94f6e5da382071ef94a45bff50
shymoj <smojtaba@mail.sfsu.edu>
2018-05-06 18:31:39 -0700

    firebase login working
    ya

---
+++ Study Sesh/Study Sesh/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  FireSwiftAuthentication
+//
+//
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var lblCaution: UILabel!
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
+    
+    var handle: FIRAuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
+            print(auth)
+            if let user = user {
+                print(user)
+       
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        FIRAuth.auth()?.removeStateDidChangeListener(handle!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginBtn(_ sender: UIButton) {
+        let user = self.txtUser.text
+        let password = self.txtPass.text
+        
+        self.view.endEditing(true)
+        self.lblCaution?.text = ""
+        
+        FIRAuth.auth()?.createUser(withEmail: user!, password: password!) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                
+            }
+            else if let user = user {
+                print(user)
+               
+            }
+        }
+    }
+ }
+ 
+ 
---
+++ Study Sesh/Study Sesh/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupControllerView.swift
+//  Study Sesh
+//
+//  Created by Shyran on 5/6/18.
+//  Copyright © 2018 Mertilien Studios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignupViewController: UIViewController {
+
+}
