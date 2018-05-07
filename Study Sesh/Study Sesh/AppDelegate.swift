9390ba15c7b778f5c79a283e7b44eff0768461ab
shymoj <smojtaba@mail.sfsu.edu>
2018-05-06 18:31:39 -0700

    firebase login working
    ya

--- Study Sesh/Study Sesh/AppDelegate.swift
+++ Study Sesh/Study Sesh/AppDelegate.swift
@@ -10,14 +10,15 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        //FIRApp.configure()
+       FIRApp.configure()
+        
         // Override point for customization after application launch.
         FIRApp.configure()
         
